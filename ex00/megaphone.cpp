f7b682a37e55e491cec0e7604cddf785b3cc6095
Robin Rollin <10898753+Sr0bin@users.noreply.github.com>
2026-01-21 16:31:13 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,15 +6,31 @@
 /*   By: rorollin <rorollin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 16:04:05 by rorollin          #+#    #+#             */
-/*   Updated: 2026/01/21 15:19:34 by rorollin         ###   ########.fr       */
+/*   Updated: 2026/01/21 16:30:07 by rorollin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <iostream>
 
-int	main()
+static std::string	upper_string(std::string input)
 {
-	std::cout << "test";
+	for (size_t i = 0; i < input.size(); ++i)
+	{
+		input[i] = std::toupper(input[i]);
+	}
+	return (input);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc <= 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < argc; ++i)
+		std::cout << upper_string(argv[i]);
+	std::cout << '\n';
 	return (0);
 }
